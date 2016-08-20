68a8b3e51def31862f6fcde14cba2f582212bfcc
Drew <deeringa@gmail.com>
2016-08-20 18:19:42 -0500

    created custom directive for weather report

--- app.config.js
+++ app.config.js
@@ -2,7 +2,9 @@ var config = {
   APIURL_CURRENT: "//api.openweathermap.org/data/2.5/weather",
   APIURL_FORECAST: "//api.openweathermap.org/data/2.5/forecast/daily",
   APIKEY: "000684b9c4555509b97b981b348a181e",
-  DATAMODE: 'json',
-  DAYCOUNT: 15,
-  UNITS: 'Kelvin' // Imperial, Metric, Kelvin (default)
+  CITY: "Parkville, MO",
+  COUNTRY: "US",
+  DATAMODE: "json",
+  DAYCOUNT: "7",
+  UNITS: "Kelvin" // Imperial, Metric, Kelvin (default)
 }
--- app.js
+++ app.js
@@ -5,23 +5,27 @@ var forecastApp = angular.module('forecastApp', ['ngResource', 'ngRoute']);
 forecastApp.config(function ($routeProvider) {
   $routeProvider
   .when('/', {
-    templateUrl: 'pages/index.htm',
+    templateUrl: 'routes/index.htm',
     controller: 'mainCtrl'
   })
   .when('/forecast', {
-    templateUrl: 'pages/forecast.htm',
+    templateUrl: 'routes/forecast.htm',
+    controller: 'forecastCtrl'
+  })
+  .when('/forecast/:num_days', {
+    templateUrl: 'routes/forecast.htm',
     controller: 'forecastCtrl'
   })
   .when('/current', {
-    templateUrl: 'pages/current.htm',
+    templateUrl: 'routes/current.htm',
     controller: 'currentCtrl'
   });
 });
 
 // CUSTOM SERVICES
 forecastApp.service('locService', function() {
-  this.location = 'Parkville, MO';
-  this.country = 'US';
+  this.location = config.CITY;
+  this.country = config.COUNTRY;
 });
 
 forecastApp.service('conversionService', function() {
@@ -39,10 +43,10 @@ forecastApp.service('conversionService', function() {
     }
   };
   
-  this.secondsToDate = function (seconds) {
-    var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
-    d.setUTCSeconds(seconds);
-    return d.toDateString();
+  this.toDate = function (ms) {
+    return new Date(ms*1000);
+    // .toDateString(); - could append .toDateString(); to the new Date() and have it output
+    // a correct date string, but let's do this the Angular way in the view.
   }
 });
 
@@ -54,7 +58,37 @@ forecastApp.controller('mainCtrl', ['$scope', 'locService', function ($scope, lo
   });
 }]);
 
-forecastApp.controller('currentCtrl', ['$scope', '$resource', 'locService', 'conversionService', function ($scope, $resource, locService, conversionService) {
+
+forecastApp.controller('forecastCtrl', ['$scope', '$resource', '$routeParams', 'locService', 'conversionService', function ($scope, $resource, $routeParams, locService, conversionService) {
+  
+  $scope.location = locService.location;
+  $scope.country = locService.country;
+  
+  config.DAYCOUNT = $routeParams.num_days || config.DAYCOUNT;
+  $scope.num_days = $routeParams.num_days || config.DAYCOUNT;
+  
+  $scope.convertToFahrenheit = function (deg) {
+    return conversionService.toFahrenheit(deg);
+  };
+  
+  $scope.timeConvert = function (ms) {
+    return conversionService.toDate(ms);
+  }
+  
+  $scope.openWeatherAPI = $resource(config.APIURL_FORECAST, { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+  
+  $scope.openWeatherResult = $scope.openWeatherAPI.get({
+    APPID: config.APIKEY,
+    q: $scope.location + ',' + $scope.country,
+    units: config.UNITS,
+    mode: config.DATAMODE,
+    cnt: config.DAYCOUNT
+  });
+console.log($scope.openWeatherResult);
+}]);
+
+
+forecastApp.controller('currentCtrl', ['$scope', '$resource', '$routeParams', 'locService', 'conversionService', function ($scope, $resource, $routeParams, locService, conversionService) {
   
   $scope.location = locService.location;
   $scope.country = locService.country;
@@ -63,8 +97,8 @@ forecastApp.controller('currentCtrl', ['$scope', '$resource', 'locService', 'con
     return conversionService.toFahrenheit(deg);
   };
   
-  $scope.timeConvert = function (seconds) {
-    return conversionService.secondsToDate(seconds);
+  $scope.timeConvert = function (ms) {
+    return conversionService.toDate(ms);
   }
   
   $scope.openWeatherAPI = $resource(config.APIURL_CURRENT, { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
@@ -78,27 +112,18 @@ forecastApp.controller('currentCtrl', ['$scope', '$resource', 'locService', 'con
   
 }]);
 
-forecastApp.controller('forecastCtrl', ['$scope', '$resource', 'locService', 'conversionService', function ($scope, $resource, locService, conversionService) {
-  
-  $scope.location = locService.location;
-  $scope.country = locService.country;
-  
-  $scope.convertToFahrenheit = function (deg) {
-    return conversionService.toFahrenheit(deg);
-  };
-  
-  $scope.timeConvert = function (seconds) {
-    return conversionService.secondsToDate(seconds);
-  }
-  
-  $scope.openWeatherAPI = $resource(config.APIURL_FORECAST, { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
-  
-  $scope.openWeatherResult = $scope.openWeatherAPI.get({
-    APPID: config.APIKEY,
-    q: $scope.location + ',' + $scope.country,
-    units: config.UNITS,
-    mode: config.DATAMODE,
-    cnt: config.DAYCOUNT
-  });
-  
-}]);
+
+// CUSTOM DIRECTIVES
+forecastApp.directive("dailyForecast", function() {
+   return {
+     restrict: 'E',
+     templateUrl: 'directives/daily_forecast.htm',
+     replace: true,
+     scope: {
+       day: "=",
+       convertTemp: "&",
+       convertDate: "&",
+       dateFormat: "@"
+     }
+   }
+});
